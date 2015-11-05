3ffd0d7d5872732ac3413a6f9e2b0ff50775fb00
Pavan Matta <pavan.matta@cyberoam.com>
2015-11-05 14:00:52 +0530

    first commit

---
+++ src/main/java/com/gclouderic/eric/Main.java
@@ -0,0 +1,34 @@
+package com.gclouderic.eric;
+
+import static spark.Spark.get;
+import static spark.SparkBase.port;
+import static spark.SparkBase.staticFileLocation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		//Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+     	port(Integer.valueOf(System.getenv("PORT")));
+	    staticFileLocation("/public");
+
+	    get("/hello", (req, res) -> "Hello World");
+
+	    get("/", (request, response) -> {
+	            Map<String, Object> attributes = new HashMap<>();
+	            attributes.put("message", "Hello World!");
+
+	            return new ModelAndView(attributes, "index.ftl");
+	        }, new FreeMarkerEngine());
+	}
+
+	}
+
+
---
+++ src/main/resources/spark/template/freemarker/index.ftl
@@ -0,0 +1,7 @@
+<html>
+<head>
+</head>
+<body>
+<h1> name ${message}</h1>
+</body>
+</html>
