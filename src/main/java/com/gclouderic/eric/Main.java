2893a2406e2872f9d9da4ac97a189a3cd8452f6b
Pavan Matta <pavan.matta@cyberoam.com>
2015-11-08 00:07:52 +0530

    updates1

--- src/main/java/com/gclouderic/eric/Main.java
+++ src/main/java/com/gclouderic/eric/Main.java
@@ -16,10 +16,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		//Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-     	port(Integer.valueOf(System.getenv("PORT")));
+     	//port(Integer.valueOf(System.getenv("PORT")));
 	    staticFileLocation("/public");
 
-	    get("/hello", (req, res) -> "Hello World");
 
 	    get("/", (request, response) -> {
 	            Map<String, Object> attributes = new HashMap<>();
@@ -27,6 +26,13 @@ public class Main {
 
 	            return new ModelAndView(attributes, "index.ftl");
 	        }, new FreeMarkerEngine());
+	    
+	    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
 	}
 
 	}
